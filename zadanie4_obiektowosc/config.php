ef3e27d50618171a1f2b5b241bc7030224ef7589
PatrykPlinta3 <100872133+PatrykPlinta3@users.noreply.github.com>
2022-04-12 09:25:37 +0200

    Update config.php

--- zadanie4_obiektowosc/config.php
+++ zadanie4_obiektowosc/config.php
@@ -8,5 +8,5 @@ $conf = new Config();
 $conf->root_path = dirname(__FILE__);
 $conf->server_name = 'localhost:80';
 $conf->server_url = 'http://'.$conf->server_name;
-$conf->app_root = '/zadanie4';
+$conf->app_root = '/zadanie4_obiektowosc';
 $conf->app_url = $conf->server_url.$conf->app_root;
